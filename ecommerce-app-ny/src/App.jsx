32abbaf7542b43d7fc9b0fb0e040b9349961dceb
Nyazashali <Shaik.nyazashali@gmail.com>
2025-01-03 20:32:11 +0530

    22-12

--- ecommerce-app-ny/src/App.jsx
+++ ecommerce-app-ny/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './assets/components/Navbar';
-import Footer from './assets/components/Footer';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
