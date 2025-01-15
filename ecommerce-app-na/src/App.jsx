8d4c5ce6281f6b1a62ed20e6451b2e3f88b631a3
Nyazashali <Shaik.nyazashali@gmail.com>
2025-01-15 20:47:58 +0530

    react-app

--- ecommerce-app-na/src/App.jsx
+++ ecommerce-app-na/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome to react js </h1>
+
+      <Navbar/>
+      <Footer/>
+
     </>
   )
 }
 
-export default App
+export default App  //22-12-2024 41:36
