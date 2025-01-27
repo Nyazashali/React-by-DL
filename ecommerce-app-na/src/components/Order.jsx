cff357315859202b73dc67654eac8eea175c9a76
Nyazashali <Shaik.nyazashali@gmail.com>
2025-01-27 18:10:08 +0530

    react

--- ecommerce-app-na/src/components/Order.jsx
+++ ecommerce-app-na/src/components/Order.jsx
@@ -3,19 +3,19 @@ function Order(props) {
     console.log(order);
     return (
         <div className="shadow p-4 bg-gray-100">
-            <div className="border-b bg-gray-300"> 
+            <div> 
                 <h2>OrderId: <span>{order.orderId}</span></h2>
             </div>
-            <div className="border-b bg-gray-300">
+            <div>
                 <h2>CustomerName: <span>{order.customerName}</span></h2>
                 <h2>Total: <span>{order.totalAmount}</span></h2>
             </div>
-            <div className="border-b bg-gray-300">
+            <div>
                 <h2>Items</h2>
                 {order.items.map(items => {
                     <h2>{items.name} - {items.price}</h2>
                 })}
-                <div className="border-b bg-gray-300">
+                <div>
                     <h2>OrderDate: {order.orderDate}</h2>
                 </div>
             </div>
